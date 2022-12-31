14fc706c9e7fd0d1722c5dc193881416f34bf5fb
Mitch Chanza <mitch.chanza@gmail.com>
2022-12-31 12:41:56 +0200

    feat: add opacity system fixes #4

--- src/dev/index.ts
+++ src/dev/index.ts
@@ -1,11 +1,13 @@
 import {gridOptions} from "./grid";
 import {textContent} from "./text";
 import {backgroundContent} from "./background";
+import {opacity} from "./background/opacity";
 
 const root: any = document.querySelector(".root")
 // background color styles
 
 const rootElem: any=`
+
 ${
     backgroundContent()
 }
@@ -15,5 +17,10 @@ ${
 ${
     gridOptions()
 }
+
+${
+    opacity()
+}
+
 `;
 root.innerHTML = rootElem
